3958a24fc719b036c72683a848b701a2923d9751
KangHo-dev <tiger452003@gmail.com>
2023-06-24 05:30:30 +0900

    feat : 페이징 기능 추가

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
+const pagingSize = Number(10);
+const store = {
+    currentPage: 1,
+};
 
 function getData(url) {
     ajax.open('GET', url, false);
@@ -18,29 +21,36 @@ function newsFeed() {
 
     newsList.push('<ul>')
     
-    for(let i = 0; i < 10; i++) {
+    for(let i = (store.currentPage -1) * pagingSize; i < store.currentPage * pagingSize; i++) {
         newsList.push(`
             <li>
-                <a href = "#${newsFeed[i].id}">
+                <a href = "#/show/${newsFeed[i].id}">
                     ${newsFeed[i].title} (${newsFeed[i].comments_count})
                 </a>
             </li>
         `);
     }
+
+    const maxPage = Object.values(newsFeed).length / pagingSize;
     newsList.push('</ul>')
-    
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1 <= maxPage ? store.currentPage + 1 : store.currentPage}">다음 페이지</a>
+        </div>
+    `)
     container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
-    const id = location.hash.substr(1);
+    const id = location.hash.substr(7);
     const newsContent = getData(CONTENT_URL.replace('@id', id));
 
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
 
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -50,6 +60,9 @@ function router() {
     const routePath = location.hash;
 
     if (routePath === '') {
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) {
+        store.currentPage = Number(routePath.substr(7));
         newsFeed();
     } else {
         newsDetail();
