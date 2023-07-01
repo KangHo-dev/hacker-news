f9657e146aecc25ecb81ff251da254aa57cdba89
KangHo-dev <tiger452003@gmail.com>
2023-07-01 09:03:14 +0900

    refactor : ajax 요청 함수 -> 클래스 상속

--- app.ts
+++ app.ts
@@ -38,11 +38,33 @@ const store: Store = {
     feeds: [],
 };
 
-function getData<AjaxResponse>(url: string): AjaxResponse {
-    ajax.open('GET', url, false);
-    ajax.send();
+class Api {
+    url: string;
+    ajax: XMLHttpRequest;
 
-    return JSON.parse(ajax.response);
+    constructor(url: string) {
+        this.url = url;
+        this.ajax = new XMLHttpRequest();
+    }
+
+    protected getRequest<AjaxResponse>(): AjaxResponse {
+        this.ajax.open('GET', this.url, false);
+        this.ajax.send();
+
+        return JSON.parse(this.ajax.response);
+    }
+}
+
+class NewsFeedApi extends Api {
+    getData(): NewsFeed[] {
+        return this.getRequest<NewsFeed[]>();
+    }
+}
+
+class NewsDetailApi extends Api {
+    getData(): NewsDetail {
+        return this.getRequest<NewsDetail>();
+    }
 }
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
@@ -62,6 +84,7 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
+    const api = new NewsFeedApi(NEWS_URL);
     let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
 
@@ -91,12 +114,12 @@ function newsFeed(): void {
         `;
 
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+        newsFeed = store.feeds = makeFeeds(api.getData());
     }
     
     for(let i = (store.currentPage -1) * pagingSize; i < store.currentPage * pagingSize; i++) {
         newsList.push(`
-            <div class="p-6 ${newsFeed[i].read ? 'bg-red-500' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
+            <div class="p-6 ${newsFeed[i].read ? 'bg-gray-300' : 'bg-white'} mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100">
                 <div class="flex">
                     <div class="flex-auto">
                     <a href="#/show/${newsFeed[i].id}">${newsFeed[i].title}</a>  
@@ -128,7 +151,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
     const id = location.hash.substr(7);
-    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
+    const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
+    const newsContent = api.getData();
     let template = `
         <div class="bg-gray-600 min-h-screen pb-8">
             <div class="bg-white text-xl">
